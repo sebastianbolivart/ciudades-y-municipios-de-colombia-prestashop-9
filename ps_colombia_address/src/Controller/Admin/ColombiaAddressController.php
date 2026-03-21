5fe6a59d8cd9f6e156442b96ce22db0cb0ba56bb
Sebastian Bolivar <sebastianbolivartapias@gmail.com>
2026-03-21 11:42:42 -0500

    refactor: Update controller service definition and routing for improved Symfony compatibility

--- ps_colombia_address/src/Controller/Admin/ColombiaAddressController.php
+++ ps_colombia_address/src/Controller/Admin/ColombiaAddressController.php
@@ -35,6 +35,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Csrf\CsrfToken;
 
 /**
